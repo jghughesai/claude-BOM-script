7dd020f0024309341d037c0ffdade1a0ce5d64e6
jghughesai <162188970+jghughesai@users.noreply.github.com>
2024-06-28 16:57:39 -0400

    Update

--- main.py
+++ main.py
@@ -23,17 +23,12 @@ def load_prompt_from_subdir(sub_dir_path):
 def main():
     claude_obj = claude.ClaudeInterpreter(api_key=ANTHROPIC_API_KEY)
 
-    sub_dirs = [sub for sub in os.listdir(BOM_DIRECTORY) if sub.startswith('format')]
-    sub_dirs.sort(key=lambda x: int(re.search(r'\d+', x).group()))
+    print(f'sub_dir being worked: BOMs/format5\n')
 
-    for sub_dir in sub_dirs:
-        sub_dir_path = os.path.join(BOM_DIRECTORY, sub_dir)
-        print(f'sub_dir being worked: {sub_dir_path}\n')
+    prompt = load_prompt_from_subdir('BOMs/format5')
+    print(f'prompt: {prompt}\n')
 
-        prompt = load_prompt_from_subdir(sub_dir_path)
-        print(f'prompt: {prompt}\n')
-
-        process.process_dir(claude_obj, sub_dir_path, prompt, CLAUDE_OUTPUT_PATH)
+    process.process_dir(claude_obj, 'BOMs/format5', prompt, CLAUDE_OUTPUT_PATH)
 
 
 if __name__== '__main__':
